02caf510069b2d0990bc039887d244b58fc7feaa
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-11-02 00:42:50 +0200

    queries

--- queries.sql
+++ queries.sql
@@ -31,3 +31,19 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK TRANSACTION;
 SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT CH1;
+UPDATE animals SET weight_kg=weight_kg*-1;
+ROLLBACK TO CH1;
+UPDATE animals SET weight_kg=weight_kg*-1 WHERE weight_kg<0;
+COMMIT TRANSACTION;
+SELECT * FROM animals;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
+SELECT AVG(weight_kg) FROM  animals;
+SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT name,date_of_birth, AVG(escape_attempts) FROM animals GROUP BY name, date_of_birth HAVING date_of_birth BETWEEN '1990-01-01' AND '2000-01-01';
